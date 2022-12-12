60cec2c1334848b2696e5c616d06c7ef503996e4
iYannis <skalvolin@yandex.ru>
2022-12-12 17:08:58 +0300

    h w 6 done

---
+++ homework6_09dec22/Task_41_Homework6/Program.cs
@@ -0,0 +1,31 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+double[] PrintArrayFromString(string array)
+{
+    string[] elements = array.Split("/", StringSplitOptions.RemoveEmptyEntries);
+    double[] res = new double[elements.Length];
+
+    for (int i = 0; i < elements.Length; i++)
+    {
+        res[i] = double.Parse(elements[i]);
+    }
+    Console.Write($"В массиве [{string.Join("; ", res)}] ");
+    return res;
+}
+int HawManyPositivs(double[] array)
+{
+    int countPositivs = 0;
+    foreach(double item in array)
+        {
+        if(item>0) countPositivs++;
+        }
+    return countPositivs;
+}
+Console.Clear();
+Console.Write("Введите элементы массива через слэш(/) без пробелов: ");
+string numbers = Console.ReadLine();
+double[] array = PrintArrayFromString(numbers);
+Console.WriteLine($"количество положительных чисел -> {HawManyPositivs(array)}");
+
+
---
+++ homework6_09dec22/Task_43_Homework6/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+double[] ArrayFromString(string array)
+{
+    string[] elements = array.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    double[] res = new double[elements.Length];
+
+    for (int i = 0; i < elements.Length; i++)
+    {
+        res[i] = double.Parse(elements[i]);
+    }
+        return res;
+}
+double[] CrossPoint(double[] firstLine, double[] secondLine)
+{
+    double[] result = new double[2];
+    result[0] = (secondLine[1] - firstLine[1])/ (firstLine[0] - secondLine[0]);
+    if(result[0] == 0) result[0]=Math.Abs(result[0]);
+    result[1] = result[0]*firstLine[0]+firstLine[1];
+    if(result[1] == 0) result[1]=Math.Abs(result[1]);
+    return result;
+}
+
+Console.Clear();
+Console.WriteLine("Эта программа найдет координаты точки пересечения двух прямых y=k*x + b");
+Console.Write("Введите через пробел k1 и b1 для первой прямой: ");
+string first = Console.ReadLine();
+double[] firstLine = ArrayFromString(first);
+Console.Write("Введите через пробел k2 и b2 для второй прямой: ");
+string second = Console.ReadLine();
+double[] secondLine = ArrayFromString(second);
+if(firstLine.Length != 2 || secondLine.Length != 2) 
+    {
+        Console.WriteLine("Коэффициенты прямых введены с ошибкой, попробуйте еще раз");
+        return;
+    }
+if(firstLine[0] == secondLine[0]) 
+    {
+        Console.WriteLine("Эти прямые не пересекаются - они параллельны!");
+        return;
+    }
+else
+    {
+    double[]coordinateCrossPoint = CrossPoint(firstLine, secondLine);
+    Console.WriteLine($"Координаты точки пересечения этих прямых ({coordinateCrossPoint[0]:f3}; {coordinateCrossPoint[1]:f3}) (Округлено до тысячных)");
+    }
